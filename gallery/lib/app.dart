f371680aad8f5642671e0562f8a0a04477d5f109
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2019-05-31 00:36:52 +0530

    Code pushed in app.dart under stateless Widger

--- gallery/lib/app.dart
+++ gallery/lib/app.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+class app extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            print("Hi there");
+          },
+        ),
+        appBar: AppBar(
+          title: Text("Lets See Image !"),
+        ),
+      ),
+    );
+  }
+}
--- gallery/lib/main.dart
+++ gallery/lib/main.dart
@@ -1,18 +1,6 @@
 // We will import some libraries
 import 'package:flutter/material.dart';
+import 'app.dart';
 void main () {
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: (){
-          print("Hi there");
-        },
-      ),
-      appBar: AppBar(
-        title: Text("Lets See Images !"),
-      ),
-    ),
-  );
-  runApp(app);
+  runApp(app());
 }

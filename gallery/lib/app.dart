251d27432c24a6314ef2a835e572fa43133cc818
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2019-05-31 21:14:10 +0530

    Statless to Statefull

--- gallery/lib/app.dart
+++ gallery/lib/app.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
-class app extends StatelessWidget {
+
+class app extends StatefulWidget {
+  @override
+  _appState createState() => _appState();
+}
+
+class _appState extends State<app> {
+  int counter=0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text("Image Number $counter"),
+        appBar: AppBar(
+          title:Text("Images") ,),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: (){
-            print("Hi there");
+            setState(() {
+              counter++;
+            });
           },
-        ),
-        appBar: AppBar(
-          title: Text("Lets See Image !"),
         ),
       ),
     );
   }
 }
+
